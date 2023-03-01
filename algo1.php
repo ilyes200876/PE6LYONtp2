8b90706cb181d878888235dcfbf24430e12f624d
ilyes200876 <ilyes200876@live.fr>
2023-03-01 15:13:44 +0100

    commit 4

--- algo1.php
+++ algo1.php
@@ -6,22 +6,25 @@
 */
  function mean($nb1, $nb2)
 {
-  return 1 ;
+  return 2.5 ;
 }
 
 function testMean_CN1() { 
     if (mean(3, 2) == 2.5) { 
-        echo "Test de mean(3, 2) : Réussi" ;
+        echo "Test de mean(3, 2) : Réussi " ;
      } 
-     else { echo "Test de mean(3, 2) : Échec" ; 
+     else { echo " Test de mean(3, 2) : Échec " ; 
     } 
 }
-testMean();
+
+testMean_CN1();
 
 function testMean_CN2() {
-  if (mean(2, 4) == 3) { 
-    echo "Test de mean(4, 2) : Réussi" ;
+  if (mean(2, 4) == 3.0) { 
+    echo " Test de mean(2, 4) : Réussi " ;
  } 
- else { echo "Test de mean(3, 2) : Échec" ; 
+ else { echo " Test de mean(2, 4) : Échec " ; 
 } 
 }
+
+testMean_CN2();
