fd8e71130b9d4fadc75516273ee89f8f4049ef65
Mi-Seun <misun.jang73@gmail.com>
2023-03-01 15:18:27 +0100

    commit 6

--- algo1.php
+++ algo1.php
@@ -3,10 +3,11 @@
 /*
   CN1 : mean(2,3) => 2.5
   CN2 : mean(2,4) => 3
+  CN3 : mean(5, 6) => 5.5
 */
  function mean($nb1, $nb2)
 {
-  return 2.5 ;
+  return 3 ;
 }
 
 function testMean_CN1() { 
@@ -28,3 +29,13 @@ function testMean_CN2() {
 }
 
 testMean_CN2();
+
+function testMean_CN3() {
+  if (mean(5, 6) == 5.5) { 
+    echo " Test de mean(5, 6) : Réussi " ;
+ } 
+ else { echo " Test de mean(5, 6) : Échec " ; 
+} 
+}
+
+testMean_CN3();
